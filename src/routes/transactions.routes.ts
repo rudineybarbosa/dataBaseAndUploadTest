c5753087425dc6a329492d9367421cfcc7432e0d
rudiney <rudiney1h@gmail.comm>
2020-05-19 09:40:16 -0300

    adding simple input validations

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import AppError from '../errors/AppError';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
@@ -11,7 +12,41 @@ transactionsRouter.get('/', async (request, response) => {
   return response.json({ ok: 'rudiney get' });
 });
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function validate(
+  title: string,
+  type: string,
+  value: number,
+  category: string,
+) {
+  if (title === undefined || title === '') {
+    throw new AppError('Title is required');
+  }
+
+  if (type === undefined || type === '') {
+    throw new AppError('Title is required');
+  }
+
+  if (type !== 'income' && type !== 'outcome') {
+    throw new AppError('Type invalid');
+  }
+
+  if (value === undefined || value === 0) {
+    throw new AppError('Value is required');
+  }
+  if (category === undefined || category === '') {
+    throw new AppError('Category is required');
+  }
+}
+
 transactionsRouter.post('/', async (request, response) => {
+  const { title, type, value, category } = request.body;
+
+  validate(title, type, value, category);
+
+  const transaction = 
+
+
   return response.json({ ok: 'rudiney post' });
 });
 
